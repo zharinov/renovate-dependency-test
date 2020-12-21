fc010ae2daed6a9151bb678be6a35855210e576d
N0tMyFaultOG <mc.cache@web.de>
2020-12-21 22:20:37 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # renovate-dependency-test
+
+A basic repository testing out Renovate's dependency detection on shaded/relocated dependencies by relocating bStats, the metrics tool Minecraft Bukkit plugins.
---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+import org.cadixdev.gradle.licenser.LicenseExtension
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    id("java")
+    id("org.cadixdev.licenser") version "0.5.0"
+    id("net.minecrell.plugin-yml.bukkit") version "0.3.0"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    kotlin("jvm") version "1.4.21"
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = sourceCompatibility
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+repositories {
+    maven {
+        name = "Paper"
+        url = uri("https://papermc.io/repo/repository/maven-public/")
+    }
+    maven {
+        name = "bStats"
+        url = uri("https://repo.codemc.org/repository/maven-public")
+    }
+}
+
+dependencies {
+    compileOnly("com.destroystokyo.paper:paper-api:1.16.4-R0.1-SNAPSHOT")
+    implementation("org.bstats:bstats-bukkit:1.8")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+version = "1.0.0"
+
+tasks.named<ShadowJar>("shadowJar") {
+    archiveClassifier.set(null as String?)
+    dependencies {
+        include(dependency("org.bstats:bstats-bukkit:1.8"))
+        relocate("org.bstats", "de.n0tmyfaultog.metrics")
+    }
+}
+
+configure<LicenseExtension> {
+    header = rootProject.file("LICENSE-HEADER")
+    newLine = false
+}
+
+bukkit {
+    name = "RenovateDependencyTest"
+    main = "de.n0tmyfaultog.renovatedependencytest.RenovateDependencyTestPlugin"
+    author = "NotMyFault"
+    apiVersion = "1.13"
+    version = rootProject.version.toString()
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "11"
+}
+
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "11"
+}
+
+tasks.named("build").configure {
+    dependsOn("shadowJar")
+}
